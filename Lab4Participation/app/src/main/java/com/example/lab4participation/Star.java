2ca92c4071dc400a4da4e5c3e246ad8990d16890
zhuoweiyan <71348896+zhuoweiyan@users.noreply.github.com>
2022-02-01 21:12:50 -0700

    Star

---
+++ Lab4Participation/app/src/main/java/com/example/lab4participation/Star.java
@@ -0,0 +1,16 @@
+package com.example.lab4participation;
+
+public class Star extends Shape{
+
+
+    public Star(int x, int y) {
+        super(x, y);
+    }
+
+    public Star(){
+        super(0,0);
+
+    }
+
+
+}
