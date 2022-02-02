a11406426b2de906f4610e7a91506264d9f397a9
Iyury1 <iyurychu@ualberta.ca>
2022-02-01 20:10:23 -0700

    changed color

--- Lab4Participation/app/src/main/java/com/example/lab4participation/Shape.java
+++ Lab4Participation/app/src/main/java/com/example/lab4participation/Shape.java
@@ -3,7 +3,7 @@ package com.example.lab4participation;
 public class Shape {
     private int x;
     private int y;
-    private String colour = "colour";
+    private String colour = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
